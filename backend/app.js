cfa7e710cad2ed37ae5054399db35b0eee5fa06b
keyrun-14 <98756285+keyrun-14@users.noreply.github.com>
2022-06-01 23:11:56 +0530

    code pushed of app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,19 @@
 const express= require("express");
+var cors = require('cors')
 const app= express();
-app.use("express")
+app.use(cors())
+require("./database/database");
+const orders=require("./Schemas/OrderListSchema.js")
+app.get("/getOrder",async(req,res)=>{
+    try{
+        // console.log(orders,".......")
+        const result = await orders.find()
+                res.status(200).json(result);
 
-app.listen(8080)
+    }catch(err){
+        res.status(500).send("server error")
+
+    }
+})
+
+app.listen(8080, () => console.log("server connected " + 8080))
